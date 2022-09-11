5c85f3cae3c2dc557d70d081c3961ebe3eadd886
MetaWild <jsalaa2@gmail.com>
2022-09-11 12:14:56 -0600

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7fefe996-0443-4364-a10f-87ed5fe8e3f1';
+export const MODAL_CLOSE_SEC = 2.5;
