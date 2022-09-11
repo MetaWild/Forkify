af072df3dbeb2c9713d4e4b7ac05cd35393fa6f4
MetaWild <jsalaa2@gmail.com>
2022-09-11 13:18:30 -0600

    add numberToFraction Function

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import View from './View.js';
 import icons from 'url:../../img/icons.svg';
-import { fracty } from 'fracty';
+import { numberToFraction } from '../helpers.js';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -129,7 +129,7 @@ class RecipeView extends View {
                   <use href="${icons}#icon-check"></use>
                 </svg>
                 <div class="recipe__quantity">${
-                  ing.quantity ? fracty(ing.quantity).toString() : ''
+                  ing.quantity ? numberToFraction(ing.quantity).toString() : ''
                 }</div>
                 <div class="recipe__description">
                   <span class="recipe__unit">${ing.unit}</span>
